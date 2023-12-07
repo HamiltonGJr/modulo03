0149c7279e3a864d5b22ce08461f262a6e507d3b
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2023-12-06 23:01:47 -0300

    Config conexão banco de dados

---
+++ exercicios/exercicio_victor_02/src/database/Configdatabase.ts
@@ -0,0 +1,15 @@
+
+import { connect, connection } from "mongoose";
+import 'dotenv/config'
+
+export const conexao = async () => {
+  try{
+    connection.on( 'open', () => {
+      console.log( 'Conectado ao banco de dados - Projeto User' )
+    } )
+
+    await connect( process.env.URI as string )
+  } catch( error ) {
+    console.log( error )
+  }
+}
